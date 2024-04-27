52a1bdfd0944d8594851404cf811f015e5f2a98f
Ricardo Gomez <gduran.ricardo@gmail.com>
2024-04-27 00:02:17 -0600

    route get user by id moved to api routes

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,11 +5,19 @@ const transporter = require("../utils/mailer");
 
 module.exports = {
   getbyId: async (req, res, next) => {
-    const { id } = req.params;
-    let selectuser = await User.findById(id);
-    if (!selectuser)
-      next({ status: 404, send: { msg: "Usuario no encontrado" } });
-    next({ status: 201, send: { msg: "Usuario encotrado", data: selectuser } });
+    // const { id } = req.params;
+    try {
+      let selectuser = await User.findById(req.params.id);
+      if (!selectuser) {
+        next({ status: 404, send: { msg: "Usuario no encontrado" } });
+      }
+      next({
+        status: 201,
+        send: { msg: "Usuario encotrado", data: selectuser },
+      });
+    } catch (error) {
+      next({ status: 400, send: { msg: "Usuario no creado", err: error } });
+    }
   },
 
   createAccount: async (req, res, next) => {
